59fae2c18463426c65c8e3f5f431e0b33eb18556
Guilherme Simoes <guilherme.rdems@gmail.com>
2016-06-01 21:31:28 +0100

    Remove current-section related code
    This feature made little sense given that this CV is meant to be no longer than
    a page.

--- javascripts/application.js
+++ javascripts/application.js
@@ -43,33 +43,3 @@ $(window).scroll(function() {
         $(".js-gotop-button").fadeOut();
     }
 });
-
-/* Based on: http://css-tricks.com/snippets/jquery/detect-first-visible-element/ */
-var text, id, lastText, $current = $(".current-section");
-$(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    var first = false;
-    lastText = text;
-    $("h2 span").each(function() {
-        var offset = $(this).offset();
-        if (($(this).height() + offset.top) < (scrollTop + windowHeight) && first === false) {
-            if (($(this).height() + offset.top) < scrollTop) {
-                text = $(this).text();
-                id = $(this).siblings(".header-link").attr("href");
-            } else {
-                text = "";
-            }
-        } else {
-            first = false;
-        }
-    });
-    if (lastText !== text && text !== "Summary") {
-        if (text === "") {
-            $current.fadeOut(function() { $(this).text(""); });
-        } else {
-            $current.attr('href', id).text(text);
-            $current.fadeIn();
-        }
-    }
-});
